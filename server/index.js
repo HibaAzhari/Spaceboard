8cdba322f0f6c199e1924b08c86550bfa5bb30b0
hiba <hiba.azhari@gmail.com>
2021-11-28 10:45:40 +0800

    Remove some junk

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const TOKEN = "AAAAAAAAAAAAAAAAAAAAAOR7WAEAAAAAWv9%2Fx7exJUqtB3Bqm7VEeMuDB%2F0%3
 
 const app = express();
 const client = new TwitterApi(TOKEN
-    // appKey: "HPRrQrTnQ5DHyO5jTY04j8wp4",
-    // appSecret: "GKFjtap12CVnpMNehmsD0Fus9yF2SCYSMNUlNCaReiSLZe3R7H"
 )
 const server = http.createServer(app);
 
@@ -18,34 +16,14 @@ const spaceUrl = "http://api.twitter.com/2/spaces/search?query=nft&state=live"
 async function getSpaces() {
     const data = await client.v2.searchSpaces({ query: 'nft', 'space.fields': ['started_at', 'title', 'lang'], 'user.fields': ['name', 'username', 'profile_image_url'], state:'live' })
     console.log(data);
-    // {
-    //     headers: {
-    //         Authorization: `Bearer ${TOKEN}`
-    //     }
-    // }, (res) => {console.log(res)})
-    // return response
 }
 async function run() {
     try {    
-        console.log("yes")
         getSpaces();
-        console.log(" ################## DONE ################")
     } catch(error) {
-        console.log("oops")
         console.log(error)
         process.exit(1)
     }
 }
 
 run()
-// server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-// async () => {
-//     try {
-//         await getSpaces()
-//         console.log("here")
-//     } catch(error) {
-//         console.log("oops")
-//         console.error(object)
-//         process.exit(1)
-//     }
-// }
